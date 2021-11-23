2d9b48e822fe7dfa99229db6b94e74626586a51e
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2021-11-23 14:30:16 -0800

    Added local storage save functionality

--- Assets/script.js
+++ Assets/script.js
@@ -19,40 +19,58 @@ function createSchedule() {
     for (var i = 0; i < hourArray.length; i++) {
     newRow = document.createElement("div")
     newRow.classList ='row'
-    console.log(newRow)
     hour = document.createElement("div")
     hour.classList = "col hour"
+    hour.setAttribute("data-hour", i)
     hour.textContent = hourArray[i]
     // hour.setAttribute('')
     taskSpace = document.createElement("textarea")
     taskSpace.classList = 'col-md-8'
     taskSpace.setAttribute("data-text", i)
-    console.log(taskSpace)
     iconSpace = document.createElement('div')
     button =document.createElement('button')
     iconSpace.append(button)
     iconSpace.classList = 'col saveBtn'
-
-    console.log(iconSpace)
     newRow.append(hour);
     newRow.append(taskSpace);
     newRow.append(iconSpace);
     maindiv.append(newRow)
-}}
+}
+  populatePage();
+}
 
 const clickHandler = function(event) {
   // Check to see if the element is clicked.
  if (event.target.matches('button')){
-   if(event.target.parentNode.previousSibling.textContent=""){
-     console.log('empty')
-   } else (event.target.parentNode.previousSibling.textContent!=="")
-    localStorage.setItem(event.target.parentNode.previousSibling.getAttribute("data-text"),JSON.stringify(event.target.parentNode.previousSibling.textContent))
-    console.log("success")
-    console.log(localStorage) 
-  }
+  localStorage.setItem(event.target.parentNode.previousSibling.getAttribute("data-text"),JSON.stringify(event.target.parentNode.previousSibling.value))
+};
 };
 
-date = moment().format('MMMM Do YYYY, h:mm:ss a');
+const populatePage = function() {
+  let arr= document.querySelectorAll(`[data-text]`);
+  console.log(arr)
+  for (let i=0; i<arr.length; i++){
+    let index = i.toString()
+     let keyName = localStorage.getItem(index)
+     arr[i].value = keyName
+  }
+  checkDate()
+}
+
+var date = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(date)
+
+const checkDate = function() {
+  let arr= document.querySelectorAll(`[data-hour]`);
+  console.log(arr)
+  for (let i=0; i<arr.length; i++){
+    if (arr[i].textContent < date ){
+    arr[i].classList.add('past')
+  } else if (arr[i].textContent == date ){
+     arr[i].classList.add('present')
+  } else {arr[i].classList.add('future')}
+}
+}
 
 // Add the click handler to the container that holds our buttons.
 maindiv.addEventListener('click', clickHandler);
